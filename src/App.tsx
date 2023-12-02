82846724decb22b613df800a33527180f97689db
Hamel Yesid Barrero Castaño <hamel_-barrero@hotmail.com>
2023-12-02 12:03:12 -0500

    add finished crud basic

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import DangerIcon from './components/ui/icons/DangerIcon';
 import EyeIcon from './components/ui/icons/EyeIcon';
 import TrashIcon from './components/ui/icons/TrashIcon';
 import Modal from './components/ui/modal/Modal';
-import {
-  deleteNoteService,
-  listNoteService,
-  readNoteService,
-} from './services/note.service';
+import { deleteNoteService, listNoteService } from './services/note.service';
 import { Note } from './types/note.type';
 import { parseDateUtil } from './utils/date.util';
 
@@ -20,6 +16,7 @@ const App = () => {
   const [openModal, setOpenModal] = useState(false);
   const [openModalViewNote, setOpenModalViewNote] = useState(false);
   const [openModelRemoveNote, setOpenModelRemoveNote] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleAddNoteClick = () => {
     setOpenModal(true);
@@ -56,8 +53,10 @@ const App = () => {
   };
 
   useEffect(() => {
-    readNoteService(2).then((res) => console.log({ res }));
-    listNoteService().then((res) => setNotes(res));
+    setLoading(true);
+    listNoteService()
+      .then((res) => setNotes(res))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
@@ -70,33 +69,41 @@ const App = () => {
           Add note
         </button>
         <section className="flex flex-col mt-8 w-[30rem] rounded-2xl overflow-auto shadow-md h-[24.3rem] scroll">
-          {notes.map((note, index) => (
-            <article
-              key={note.noteId}
-              className={`relative flex flex-col bg-white p-6 cursor-pointer duration-300x gap-3 h-24 ${
-                index !== notes.length - 1 ? 'border-b border-gray-300' : ''
-              } hover:bg-gray-200`}
-              style={{ marginBottom: '1px' }}
-              onClick={() => handleNoteClick(note)}
-            >
-              <div className="absolute top-1 right-2 flex items-center justify-center gap-1">
-                <button onClick={(e) => handleViewNoteClick(e, note)}>
-                  <EyeIcon />
-                </button>
-                <button
-                  onClick={(e) => handleDeleteNoteClick(e, note)}
-                  className="text-red-600"
+          {loading ? (
+            <div className="w-full h-full grid place-items-center">
+              <div className="loader"></div>
+            </div>
+          ) : (
+            <>
+              {notes.map((note, index) => (
+                <article
+                  key={note.noteId}
+                  className={`relative flex flex-col bg-white p-6 cursor-pointer duration-300x gap-3 h-24 ${
+                    index !== notes.length - 1 ? 'border-b border-gray-300' : ''
+                  } hover:bg-gray-200`}
+                  style={{ marginBottom: '1px' }}
+                  onClick={() => handleNoteClick(note)}
                 >
-                  <TrashIcon size={19} />
-                </button>
-              </div>
-              <div className="flex justify-between">
-                <h6>{note.title}</h6>
-                <p>{parseDateUtil(new Date(note.createdAt!))}</p>
-              </div>
-              <p>{note.description}</p>
-            </article>
-          ))}
+                  <div className="absolute top-1 right-2 flex items-center justify-center gap-1">
+                    <button onClick={(e) => handleViewNoteClick(e, note)}>
+                      <EyeIcon />
+                    </button>
+                    <button
+                      onClick={(e) => handleDeleteNoteClick(e, note)}
+                      className="text-red-600"
+                    >
+                      <TrashIcon size={19} />
+                    </button>
+                  </div>
+                  <div className="flex justify-between">
+                    <h6>{note.title}</h6>
+                    <p>{parseDateUtil(new Date(note.createdAt!))}</p>
+                  </div>
+                  <p>{note.description}</p>
+                </article>
+              ))}
+            </>
+          )}
         </section>
         {openModal && (
           <FormNote setOpenModal={setOpenModal} note={selectedNote} />
